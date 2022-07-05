d8e6272478396eadab8225e1971bd41488a05fa0
Chy <1552358695@qq.com>
2022-07-05 12:34:34 +0800

    学习了setup,reactive,ref函数

---
+++ v3_demo01/01-setup函数/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    根组件---{{str}}
+    <hr>
+    <button @click="sayHi">hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // composition api的使用，需要配置setup函数
+  /* 
+    1. setzaiup函数是一个新的组件选项，作为组件中compositionAPI的起点入口
+    2. 从生命周期角度来看，setup会在beforeCreate钩子函数之前执行
+    3. setup中不能使用this，this指向undefined
+    4. 所有的属性方法都写在setup函数中，在模板中需要使用的数据和函数，需要在setup中返回
+  */
+ setup() {
+  console.log('setup函数执行啦',this)
+  const str = '哈哈'
+  const sayHi = () => {
+    console.log('HelloVue3World')
+  }
+  return {
+    str,
+    sayHi
+  }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ v3_demo01/02-reactive函数/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div>{{person.name}}---{{person.age}} <button @click="addAge">长一岁</button> </div>
+    <div>{{obj.num}} <button @click="addNum">数字加</button> </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  /* 
+    1. setup 需要有返回值, 只有返回的值才能在模板中使用
+    2. 默认普通的数据, 不是响应式的
+    将 复杂数据类型转化为响应式数据，使用reactive函数
+  */
+ setup() {
+  // 复杂数据类型
+  const person = reactive({
+    name: 'Tt',
+    age: 22
+  })
+  const addAge = () => {
+    person.age ++
+  }
+  // 如果是简单数据类型，则需要转为复杂数据类型再使用reactive
+  let obj = reactive({num: 100})
+  const addNum = () => {
+    obj.num ++
+  }
+  return {
+    person,
+    obj,
+    addAge,
+    addNum
+  }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
