3024fcf90f6462a3c51431cea8c0ad4c14715235
Chy <1552358695@qq.com>
2022-07-07 22:11:42 +0800

    vue-router使用

---
+++ v3_demo01/15-路由/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+// 编程式导航 从vue2中： this.$router.push()
+// vue3中调用useRouter方法
+import { useRoute, useRouter } from 'vue-router'
+// useRoute: 路由信息对象； useRouter: 路由实例对象
+const router = useRouter()
+const route = useRoute()
+
+console.log('router', router)
+console.log('route', route)
+const goLogin = () => {
+  router.push('/login')
+
+}
+</script>
+<template>
+<div>
+  <h1>Home组件</h1>
+  <button @click="goLogin">去登陆</button>
+</div>
+</template>
+<style></style>
---
+++ v3_demo01/15-路由/views/Login.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+<div>Login组件</div>
+</template>
+<style></style>
