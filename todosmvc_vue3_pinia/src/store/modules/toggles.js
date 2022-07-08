ac2d978cfee414297ad8cebb2d94a73e66073aa2
Chy <1552358695@qq.com>
2022-07-08 12:45:04 +0800

    完成

--- todosmvc_vue3_pinia/src/store/modules/toggles.js
+++ todosmvc_vue3_pinia/src/store/modules/toggles.js
@@ -2,11 +2,14 @@ import { defineStore } from 'pinia'
 export default defineStore('toggles', {
   state () {
     return {
+      // 提供按钮选项
       btns: ['All', 'Active', 'Compelted'],
+      // 默认选中
       activeBtn: 'All'
     }
   },
   actions: {
+    // 点击切换时，切换选中按钮
     changeActive(str) {
       this.activeBtn = str
     }
