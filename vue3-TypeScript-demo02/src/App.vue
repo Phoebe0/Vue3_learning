804f17a0603006adea630c1491498ce6a59ab544
Chy <1552358695@qq.com>
2022-07-11 08:57:05 +0800

    初始化

--- vue3-TypeScript-demo02/src/App.vue
+++ vue3-TypeScript-demo02/src/App.vue
@@ -1,3 +1,10 @@
-<script lang="ts" setup></script>
-<template></template>
+<script lang="ts" setup>
+// ts与模板结构关联
+// 1. 创建空的ref
+// 2. 和模板结构关联
+// 3. 使用
+</script>
+<template>
+<h2>我是标题</h2>
+</template>
 <style></style>
